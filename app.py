ec9ec9ed8fcf2222de06f21023d203f3feb69eb9
Harsh Agrawal <harshjti0307@gmail.com>
2026-06-09 13:24:25 +0530

    Fixed dataset filename case

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title="Bird Species EDA Dashboard", layout="wide")
+st.set_page_config(
+    page_title="Bird Species EDA Dashboard",
+    layout="wide"
+)
 
 st.title("🦜 Bird Species Monitoring Dashboard")
 
 forest_sheets = pd.read_excel(
-    "Bird_Monitoring_Data_FOREST.xlsx",
+    "Bird_Monitoring_Data_FOREST.XLSX",
     sheet_name=None
 )
 
 grass_sheets = pd.read_excel(
-    "Bird_Monitoring_Data_GRASSLAND.xlsx",
+    "Bird_Monitoring_Data_GRASSLAND.XLSX",
     sheet_name=None
 )
 
